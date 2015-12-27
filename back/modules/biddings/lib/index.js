16d5c6c6b461fe0892af65c4fdfeef309ecfbc8c
Yaniv Keinan <yanivkeinan@gmail.com>
2015-12-27 14:04:36 +0200

    create and get bidding

--- back/modules/biddings/lib/index.js
+++ back/modules/biddings/lib/index.js
@@ -1,25 +1,42 @@
-var uuid = require('node-uuid');
+'use strict';
+
+var  AWS     = require('aws-sdk'),
+    uuid    = require('node-uuid');
+
+var dynamoConfig = {
+  sessionToken:    process.env.AWS_SESSION_TOKEN,
+  region:          process.env.AWS_REGION
+};
+var dynamodbDocClient = new AWS.DynamoDB.DocumentClient(dynamoConfig);
+var tableName = 'slant-biddings-' + process.env.SERVERLESS_DATA_MODEL_STAGE;
 
 module.exports.createBidding = function(event, cb) {
 
-  var response = {
+  var newBidding = {
     "id": uuid.v4(),
+    "active": true,
     "createdAt": Date.now()
   };
-
-  return cb(null, response);
+  var params = {
+    TableName : tableName,
+    Item: newBidding
+  };
+  function response() {
+    return cb(null, newBidding);
+  }
+  dynamodbDocClient.put(params, response);
 };
 
 module.exports.getBidding = function(event, cb) {
 
-  var response = {
-    "id": event.id,
-    "active": true,
-    "createdAt": Date.now(),
-    "event": event
+  var params = {
+    TableName : tableName,
+    Key: {
+      id: event.id
+    }
   };
 
-  return cb(null, response);
+  return dynamodbDocClient.get(params, cb);
 };
 
 module.exports.getBiddingContributions = function(event, cb) {
