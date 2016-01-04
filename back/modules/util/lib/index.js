2e71adf9fc80b60be7074f1b46fc89a20922eed2
Adam Goldman <adamgoldman7@gmail.com>
2016-01-04 18:44:06 +0200

    add cleanse db function

--- back/modules/util/lib/index.js
+++ back/modules/util/lib/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 module.exports = {
+  cleanseDb: cleanseDb,
   syncCachedSystemRep: syncCachedSystemRep,
   cacheTotalUsersRep: cacheTotalUsersRep,
   cacheTotalRep: cacheTotalRep
 }
 
 var _    = require('underscore');
+var async = require('async');
 var util = require('../');
 
 function cacheTotalRep(event, cb) {
@@ -45,4 +47,88 @@ function cacheTotalUsersRep(event, cb) {
 
 function syncCachedSystemRep(event, cb) {
   return cb(event);
+}
+
+function cleanseDb(event, cb) {
+  async.parallel([
+
+    function(parallelCB) {
+      async.waterfall([
+        function(waterfallCB) {
+          getAllItemsFromDb('users', waterfallCB);
+        },
+        function(users, waterfallCB) {
+          deleteItemsFromDb(users, 'users', parallelCB);
+        }
+      ]);
+    },
+
+    function(parallelCB) {
+      async.waterfall([
+        function(waterfallCB) {
+          getAllItemsFromDb('biddings', waterfallCB);
+        },
+        function(biddings, waterfallCB) {
+          deleteItemsFromDb(biddings, 'biddings', parallelCB);
+        }
+      ]);
+    },
+
+    function(parallelCB) {
+      async.waterfall([
+        function(waterfallCB) {
+          getAllItemsFromDb('contributions', waterfallCB);
+        },
+        function(contributions, waterfallCB) {
+          deleteItemsFromDb(contributions, 'contributions', parallelCB);
+        }
+      ]);
+    },
+
+    function(parallelCB) {
+      async.waterfall([
+        function(waterfallCB) {
+          getAllItemsFromDb('evaluations', waterfallCB);
+        },
+        function(evaluations, waterfallCB) {
+          deleteItemsFromDb(evaluations, 'evaluations', parallelCB);
+        }
+      ]);
+    }
+
+  ], function(err, results) {
+    cb(err, results);
+  });
+}
+
+function getAllItemsFromDb(table, cb) {
+  var paramsForQueringUsers = {
+    TableName: util.tables[table],
+    ConsistentRead: true,
+    ReturnConsumedCapacity: "TOTAL"
+  };
+  
+  util.dynamoDoc.scan(paramsForQueringUsers, function(err, data) {
+    console.log('err', err, 'data', data);
+    if (err) return cb(err);
+    cb(err, data.Items);
+  });
+}
+
+function deleteItemsFromDb(xs, table, cb) {
+
+  async.each(xs, function(x, asyncCB) {
+    var params = {
+      TableName : util.tables[table],
+      Key: { id: x.id }
+    };
+
+    return util.dynamoDoc.delete(params, function(err, data) {
+      return asyncCB(err, params.key);
+    });
+
+  }, function(err) {
+    cb(err, xs.length + ' ' + table + ' deleted');
+  });
+
 }
